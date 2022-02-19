2e233a432f8ffbd52180bb909ca98f4736558fc9
sudoVijaySoni <vsoni73831@gmail.com>
2022-02-19 18:51:47 +0530

    Done

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const uri = "your mondo atlas link here"
+mongoose.connect(uri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((e) => {
+    console.log(`no connection`);
+})
